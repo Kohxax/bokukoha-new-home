fb3038c610ef6ce35b774e0cabba6b75d9dda503
Kohxax <koharen0256@gmail.com>
2025-11-10 17:01:55 +0900

    add ui-avatar

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,20 @@
 <template>
-    <div class="min-h-screen bg-background flex justify-center">
-        <Button>click me</Button>
+
+    <div class="relative flex min-h-screen flex-col">
+
+        <main class="flex flex-1 flex-col items-center justify-center p-4 text-center">
+
+            <Avatar class="mb-6 h-40 w-40 shadow-xl">
+                <img src="~/assets/img/icon_glass.png" alt="Koha" />
+                <AvatarFallback>KH</AvatarFallback>
+            </Avatar>
+            
+        </main>
+
     </div>
+
 </template>
+
+<script lang="ts" setup>
+import { Github } from "lucide-vue-next"
+</script>
