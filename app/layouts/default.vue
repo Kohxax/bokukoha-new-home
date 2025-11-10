2a575676908044865d3125326e8af9f246b6a152
Kohxax <koharen0256@gmail.com>
2025-11-10 16:27:52 +0900

    change ダークモード

---
+++ app/layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="min-h-screen">
+        <slot />
+    </div>
+</template>
+
+<script lang="ts" setup>
+useHead ({
+    htmlAttrs: {
+        class: 'dark',
+        lang: 'ja'
+    }
+})
+</script>
