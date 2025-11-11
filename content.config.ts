a7a912ac7cc3a53bdcb48dc8c7cd023cf9af0dc5
Kohxax <koharen0256@gmail.com>
2025-11-11 14:46:40 +0900

    add zod, スキーマの追加

--- content.config.ts
+++ content.config.ts
@@ -1,16 +1,32 @@
-import { defineContentConfig, defineCollection } from '@nuxt/content'
+import { defineContentConfig, defineCollection, z } from '@nuxt/content'
 
 export default defineContentConfig({
-  collections: {
-    blog: defineCollection({
-        type: 'page',
-        source: 'blog/*.md',
+    collections: {
+        blog: defineCollection({
+            type: 'page',
+            source: 'blog/*.md',
+            schema: z.object({
+                title: z.string(),
+                date: z.string(),
+                category: z.string(),
+                tags: z.array(z.string()).optional(),
+                coverImage: z.string(),
+                draft: z.boolean().default(true)
+            })
 
-    }),
+        }),
 
-    work: defineCollection({
-        type: 'page',
-        source: 'work/*.md',
-    }),
-  }
+        work: defineCollection({
+            type: 'page',
+            source: 'work/*.md',
+            schema: z.object({
+                title: z.string(),
+                date: z.string(),
+                category: z.string(),
+                tags: z.array(z.string()).optional(),
+                coverImage: z.string(),
+                draft: z.boolean().default(true)
+            })
+        }),
+    }
 })
