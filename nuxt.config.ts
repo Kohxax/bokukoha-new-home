9987d479d5c7c924cdbe51a442e29bab40000c88
Kohxax <koharen0256@gmail.com>
2025-11-14 12:13:54 +0900

    add ハイライト対応言語追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -86,6 +86,18 @@ export default defineNuxtConfig({
       markdown: {
         highlight: {
           theme: 'github-dark',
+          langs: [
+            'java',
+            'kotlin',
+            'json',
+            'js',
+            'ts',
+            'html',
+            'css',
+            'vue',
+            'md',
+            'yaml'
+          ],
         },
       },
     },
