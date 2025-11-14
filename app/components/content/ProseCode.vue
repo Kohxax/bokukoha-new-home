efa25b337dc81b1d3a784b20afa480b9847c9d19
Kohxax <koharen0256@gmail.com>
2025-11-14 14:25:36 +0900

    fix ProseCodeでバッククォートが表示される問題修正

---
+++ app/components/content/ProseCode.vue
@@ -0,0 +1,15 @@
+<script setup>
+</script>
+
+<template>
+  <code class="bg-muted rounded-sm px-1 text-foreground font-medium">
+    <slot />
+  </code>
+</template>
+
+<style scoped>
+code::before,
+code::after {
+  content: none !important;
+}
+</style>
