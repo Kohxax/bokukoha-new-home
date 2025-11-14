ea9ea475e293dee0ab0abb38eb163a546b303ecb
Kohxax <koharen0256@gmail.com>
2025-11-14 16:15:48 +0900

    add md用Alertコンポーネント

---
+++ app/components/content/Alert.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { AlertTriangle } from 'lucide-vue-next'
+</script>
+
+<template>
+    <div class="flex items-center p-3 rounded-lg bg-yellow-100 text-yellow-900">
+        <AlertTriangle class="h-5 w-5 mr-3 shrink-0" />
+
+        <div class="grow text-md font-bold">
+            <slot mdc-unwrap="p" />
+        </div>
+    </div>
+</template>
+
+<style scoped>
+</style>
