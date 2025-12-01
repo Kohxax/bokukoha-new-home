1a734bab30808225d30001a3f97841fe7c88af44
Kohxax <koharen0256@gmail.com>
2025-12-01 22:52:44 +0900

    fix: 下の余白

--- app/components/content/Alert.vue
+++ app/components/content/Alert.vue
@@ -3,7 +3,7 @@ import { AlertTriangle } from 'lucide-vue-next'
 </script>
 
 <template>
-  <div class="flex items-center p-3 rounded-lg bg-yellow-100 text-yellow-900 mb-3">
+  <div class="flex items-center p-3 rounded-lg bg-yellow-100 text-yellow-900 mb-6">
     <AlertTriangle class="h-5 w-5 mr-3 shrink-0" />
 
     <div class="grow text-md font-bold">
