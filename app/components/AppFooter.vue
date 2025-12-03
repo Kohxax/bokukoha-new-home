a669b095654e21f4ba1e01cbe39e9127350909f9
Kohxax <koharen0256@gmail.com>
2025-12-03 13:15:59 +0900

    fix: フッター権利表記

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -1,7 +1,8 @@
 <template>
   <footer class="w-full bg-background px-6 py-3 backdrop-blur-sm">
-    <div class="container mx-auto max-w-5xl text-center text-sm text-muted-foreground">
-      © 2025 Koha. All rights reserved.
+    <div class="container flex flex-col mx-auto max-w-5xl text-center text-sm text-muted-foreground">
+      <span>© 2025 Koha. All rights reserved.</span>
+      <span>引用している文章・画像の著作権は引用元に帰属します。</span>
     </div>
   </footer>
 </template>
