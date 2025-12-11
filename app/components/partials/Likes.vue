d1ae8175a07ffecbe4e09dd482655e7c80e907ea
Kohxax <koharen0256@gmail.com>
2025-12-11 18:40:43 +0900

    enhance: Likes呼び出し時にクラス渡せるように

--- app/components/partials/Likes.vue
+++ app/components/partials/Likes.vue
@@ -1,12 +1,19 @@
 <script setup>
 import { Heart } from 'lucide-vue-next'
 
+const props = defineProps({
+  colorClass: {
+    type: String,
+    default: 'text-foreground',
+  },
+})
+
 const likes = 1
 </script>
 
 <template>
-  <div class="container flex flex-row gap-1.5 text-foreground items-center w-10 justify-center">
-    <Heart class="w-5 h-5" />
-    <span class="text-md">{{ likes }}</span>
+  <div class="container flex flex-row gap-1.5 items-center w-10 justify-center" :class="colorClass">
+    <Heart class="w-4 h-4" />
+    <span class="text-base">{{ likes }}</span>
   </div>
 </template>
