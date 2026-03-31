d3a789224e5da09d8184db5dbce54c202c7e3427
Kohxax <koharen0256@gmail.com>
2026-03-31 09:56:59 +0900

    enhance: アラートのデザインをミニマルに

--- app/components/content/Alert.vue
+++ app/components/content/Alert.vue
@@ -3,8 +3,8 @@ import { AlertTriangle } from 'lucide-vue-next'
 </script>
 
 <template>
-  <div class="flex items-center p-3 rounded-lg bg-yellow-100 text-yellow-900 mb-6">
-    <AlertTriangle class="h-5 w-5 mr-3 shrink-0" />
+  <div class="flex items-center p-3 rounded-xl bg-muted/30 text-forground mb-6 border-yellow-300">
+    <AlertTriangle class="h-5 w-5 mr-3 shrink-0 text-yellow-300" />
 
     <div class="grow text-md font-bold">
       <slot mdc-unwrap="p" />
